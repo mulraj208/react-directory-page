79343efe28a42a11c47f2a4c11f85eecd2929d73
mulraj.g <mulraj.g@media.net>
2022-01-04 11:46:26 +0530

    Sass Setup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+import './scss/app.scss';
+
 function App() {
   return (
     <div className="app">
-      <h1 className="text-3xl font-bold">
-        Hello world!
+      <h1 className="font-euclidcircularb font-bold text-2xl leading-32px text-black mt-2 mb-4 whitespace-normal md:mt-2 md:mb-6 md:text-32px md:leading-40px px-15px">
+        Hello World
       </h1>
     </div>
   );
